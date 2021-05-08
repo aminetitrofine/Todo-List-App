9e30138214ef984e2018228bea08da582411a81c
Amine TITROFINE <amine_titrofine@um5.ac.ma>
2021-05-08 06:58:34 +0000

    add final touches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
-
 import './App.css';
 
+// Components 
+import InputTodo from './Components/InputTodo';
+import ListTodos from './Components/ListTodos';
+
 function App() {
-  return  <Fragment></Fragment>;
+  return ( <Fragment>
+    <div className="container">
+      <InputTodo />
+      <ListTodos />
+    </div>
+  </Fragment>
+  );
 }
 
 export default App;
